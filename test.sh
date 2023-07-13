01beb934a3ac42ea065df75b70fd5aa360630bfb
adamwang <adamw43081@gmail.com>
2023-07-12 21:05:54 -0700

    Tested Google Maps Route API with a simple shell script.

---
+++ test.sh
@@ -0,0 +1,22 @@
+curl -X POST -d '{
+  "origin": {
+    "address": "1 Infinite Loop, Cupertino, CA 95014"
+  },
+  "destination": {
+    "address": "1600 Amphitheatre Parkway, Mountain View, CA 94043"
+  },
+  "travelMode": "DRIVE",
+  "routingPreference": "TRAFFIC_AWARE",
+  "computeAlternativeRoutes": true,
+  "routeModifiers": {
+    "avoidTolls": true,
+    "avoidHighways": false,
+    "avoidFerries": false
+  },
+  "languageCode": "en-US",
+  "units": "IMPERIAL"
+}' \
+-H 'Content-Type: application/json' \
+-H 'X-Goog-Api-Key: AIzaSyCVempxiGlAs4J4AeAhomaaB39BC82eLz4' \
+-H 'X-Goog-FieldMask: routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline' \
+'https://routes.googleapis.com/directions/v2:computeRoutes' > results.txt
